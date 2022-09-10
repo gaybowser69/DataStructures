8cd8a8cd6b721821a08820e167c7e9070e2ee5bc
Suhas Bharadwaj <66212763+gaybowser69@users.noreply.github.com>
2022-09-10 17:51:15 +0530

    Added convert to singly LL and fixed assert in del
    Added convert to singly linked list function and changed linked_list to linked_list->length within the assert in the delete function.

--- linkedlist.h
+++ linkedlist.h
@@ -16,7 +16,7 @@ void LinkedListDestructor(struct LinkedList* linked_list);
 void append(struct LinkedList* linked_list, int index, int data);
 void delete(struct LinkedList* linked_list, int index);
 void ConvertToDoublyLinkedList(struct LinkedList* linked_list);
+void ConvertToSinglyLinkedList(struct LinkedList* linked_list);
 void printList(struct LinkedList* linked_list);
-
 
 #endif
