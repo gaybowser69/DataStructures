9dc7bb27887fe7d2d99138c92fad6efe0718ad86
Suhas Bharadwaj <66212763+gaybowser69@users.noreply.github.com>
2022-09-27 23:18:46 +0530

    Restructured code, removed unnecessary free calls.

---
+++ datastructures.h
@@ -0,0 +1,6 @@
+#include "inc/node/node.h"
+#include "inc/linkedList/linkedlist.h"
+#include "inc/stack/stack.h"
+#include "inc/queue/queue.h"
+
+//gcc datastructures.h inc/node/node.h inc/node/node.c inc/linkedList/linkedlist.h inc/linkedList/linkedlist.c inc/stack/stack.h inc/stack/stack.c inc/queue/queue.h inc/queue/queue.c main.c -o main.out
