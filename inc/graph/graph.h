649feffc19ad78fbdb30cc9558a0078da5bd39c5
Suhas Bharadwaj <66212763+gaybowser69@users.noreply.github.com>
2022-12-25 13:43:45 +0530

    Added all adjacency matrix functionality

--- inc/graph/graph.h
+++ inc/graph/graph.h
@@ -1,9 +1,41 @@
-#include "node.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct Node
+{
+    int data;
+};
+
+struct Edge
+{
+    bool** adjMatrix; //Stores all neighbours
+    int** adjList; //Stores the neighbours for the node at the nodelist index
+};
 
 struct Graph
 {
     int V;
-    struct Node** nodeList;
-    int** adjMatrix;
+    struct Node** nodeList; 
+    struct Edge* edge;
 };
 
+struct Node* NodeConstructor(int data);
+void NodeDestructor(struct Node* node);
+
+struct Graph* GraphConstructor(unsigned int V);
+void GraphDestructor(struct Graph* graph);
+
+
+void GenerateAdjMatrix(struct Graph* graph);
+void DestroyAdjMatrix(struct Graph* graph);
+
+void GenerateAdjList(struct Graph* graph); //todo
+
+bool hasEdge(struct Graph* graph, int src, int dest);
+void makeEdge(struct Graph* graph, int src, int dest);
+void deleteEdge(struct Graph* graph, int src, int dest);
+
+void printAdjMatrix(struct Graph* graph);
+void printGraph(struct Graph* graph);
